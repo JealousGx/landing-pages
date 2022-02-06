c89948459b04639b541792532e5abca6e74a4354
JealousGx <khiljimateenn@gmail.com>
2022-02-06 16:14:21 +0500

    Completed features section

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const navMenu = document.querySelector(".nav");
 const header = document.querySelector(".header");
 const featured = document.querySelectorAll(".feature-btns");
 const featureBtns = document.getElementsByClassName("feature-btns");
+const content = document.getElementsByClassName("features-content");
+const featuredContents = document.querySelectorAll(".features-content");
 
 document.querySelector(".open-icon").onclick = () => {
   header.classList.add("active");
@@ -27,13 +29,20 @@ document.querySelector(".close-icon").onclick = () => {
     : closeIcon.classList.add("active");
 };
 
-// Program to switch among the tabs
+// Program to switch the tabs
 const btnsLength = featureBtns.length;
 for (let i = 0; i < btnsLength; i++) {
   featureBtns[i].onclick = () => {
     featured.forEach((featureBtn) => {
       featureBtn.classList.remove("active");
     });
+
+    featuredContents.forEach((featuredContent) => {
+      featuredContent.classList.remove("active");
+    });
     featureBtns[i].classList.add("active");
+    content[i].classList.add("active");
   };
 }
+
+// Program to switch the tab-content
